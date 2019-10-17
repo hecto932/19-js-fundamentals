d83c5e6d92188102735b070234ea9854d2fa433c
Hector Flores <Hector_Flores@eogresources.com>
2019-10-16 22:06:24 -0700

    1 - Variables

---
+++ 1_variables.js
@@ -0,0 +1,8 @@
+// Javascript is typeless language, so you won't need to
+// to say which type is a variable.
+
+var nombre = "Hector";
+var age = 28;
+
+console.log("Hi! My name is " + nombre);
+console.log("I'm " + nombre + " years old");
