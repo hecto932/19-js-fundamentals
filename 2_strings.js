7231baddc4a1f693abae9eb8fca518d07c742a07
Hector Flores <Hector_Flores@eogresources.com>
2019-10-16 22:32:57 -0700

    2 - Strings

---
+++ 2_strings.js
@@ -0,0 +1,12 @@
+var firstname = "Hector";
+var lastname = "Flores";
+
+var nameInUppercase = firstname.toUpperCase();
+var lastnameInLowercase = lastname.toLowerCase();
+
+var nameInUppercase = firstname.charAt(0);
+var numberOfFirstnameLetters = firstname.length;
+
+var fullName = `${firstname} ${lastname.toUpperCase()}`;
+
+var str = firstname.substr(1, 2);
