19b1d0e1741c09490eb5dfad66e0b1a0ec24491b
Hector Flores <Hector_Flores@eogresources.com>
2019-10-16 22:50:57 -0700

    Number

---
+++ 3_numbers.js
@@ -0,0 +1,28 @@
+var age = 27;
+
+// age = age + 1
+age += 1;
+
+var weight = 75;
+
+// weight = weight - 2
+weight -= 2;
+
+var sandwich = 1;
+
+weight = weight + sandwich;
+
+var playSoccer = 3;
+
+weight -= playSoccer;
+
+var winePrice = 200.3;
+
+var total = Math.round(winePrice * 100 * 3) / 100;
+var totalStr = total.toFixed(3);
+var total2 = parseFloat(totalStr);
+
+var pizza = 8;
+var person = 2;
+
+var countPiecesByPerson = pizza / person;
