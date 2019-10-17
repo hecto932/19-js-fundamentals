5112458a4641338d33d4d0d4268e6f8954808737
Hector Flores <Hector_Flores@eogresources.com>
2019-10-16 22:59:30 -0700

    Functions

---
+++ 4_functions.js
@@ -0,0 +1,13 @@
+var firstName = "Hector";
+var age = 28;
+
+function printAge(name, age) {
+  console.log(`${name} tiene ${age} years old`);
+}
+
+printAge(firstName, age);
+printAge("Vicky", 28);
+printAge("Eric", 24);
+printAge("Dario", 27);
+printAge(25, "Carlos");
+printAge();
