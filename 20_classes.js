872d682f8b5307b89bd5f26b8e7760078cf08063
Hector Flores <Hector_Flores@eogresources.com>
2019-10-21 16:22:02 -0700

    Classes

---
+++ 20_classes.js
@@ -0,0 +1,19 @@
+function Person(name, lastname) {
+  this.name = name;
+  this.lastname = lastname;
+  this.age = 20;
+}
+
+Person.prototype.sayHello = function() {
+  console.log(`Hi! ${this.name} ${this.lastname}`);
+};
+
+var hector = new Person("Hector", "Flores");
+var victoria = new Person("Victoria", "Flores");
+var leslie = new Person("Hector", "Flores");
+
+console.log(hector);
+
+hector.sayHello();
+victoria.sayHello();
+leslie.sayHello();
