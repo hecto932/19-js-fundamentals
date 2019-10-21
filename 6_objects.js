54140ed13008267a24f5b18c1e3528df651099de
Hector Flores <Hector_Flores@eogresources.com>
2019-10-21 10:44:46 -0700

    Objects

---
+++ 6_objects.js
@@ -0,0 +1,19 @@
+var hector = {
+  name: "Hector",
+  lastname: "Flores",
+  age: 27
+};
+
+var vicky = {
+  name: "Vicky",
+  lastname: "Flores",
+  age: 28
+};
+
+function printNameInUpperCase({ name = "default" }) {
+  console.log(name.toUpperCase());
+}
+
+printNameInUpperCase(hector);
+printNameInUpperCase(vicky);
+printNameInUpperCase({ name: "Pepito" });
