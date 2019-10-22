23ee3999a3bc9fac1ae756d4dfe508b894faab94
Hector Flores <Hector_Flores@eogresources.com>
2019-10-21 17:41:52 -0700

    Classes and prototypes

---
+++ 22_prototypal_inheritance.js
@@ -0,0 +1,61 @@
+function Person(name, lastname) {
+  this.name = name;
+  this.lastname = lastname;
+  this.age = 20;
+}
+
+function Developer(name, lastname) {
+  this.name = name;
+  this.lastname = lastname;
+}
+
+function inheritFrom(childrenPrototype, fatherPrototype) {
+  var fn = function() {};
+  fn.prototype = fatherPrototype.prototype;
+  childrenPrototype.prototype = new fn();
+  childrenPrototype.prototype.constructor = childrenPrototype;
+}
+
+Person.prototype.sayHello = function() {
+  console.log(`Hi! ${this.name} ${this.lastname}`);
+};
+
+inheritFrom(Developer, Person);
+
+Developer.prototype.sayHello = function() {
+  console.log(`Hi! my name is ${this.name} ${this.lastname}`);
+};
+
+var hector = new Person("Hector", "Flores");
+var victoria = new Person("Victoria", "Flores");
+var leslie = new Person("Hector", "Flores");
+
+console.log(hector);
+
+hector.sayHello();
+victoria.sayHello();
+leslie.sayHello();
+
+var dev = new Developer("Victoria", "Acosta");
+
+console.log(dev);
+dev.sayHello();
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    console.log(`${this.name} make noise.`);
+  }
+}
+
+class Dog extends Animal {
+  speak() {
+    console.log(`${this.name} bark`);
+  }
+}
+
+const dog = new Dog("Asha");
+dog.speak();
