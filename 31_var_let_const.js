77bc6d7e76970ad98f5cdc6a725a69f687509fc7
Hector Flores <Hector_Flores@eogresources.com>
2019-10-28 14:28:25 -0700

    31 var_let_const

---
+++ 31_var_let_const.js
@@ -0,0 +1,39 @@
+// USING VAR
+
+var person = {
+  name: "Hector",
+  lastname: "Flores",
+  age: 29
+};
+
+function itHasMoreThanEighteen(person) {
+  if (person.age >= 18) {
+    var mensaje = "Es mayor de edad";
+  } else {
+    var mensaje = "Es menor de edad";
+  }
+
+  console.log(mensaje);
+}
+
+itHasMoreThanEighteen(person);
+
+// USING LETS
+var person = {
+  name: "Hector",
+  lastname: "Flores",
+  age: 29
+};
+
+function itHasMoreThanEighteenLet(person) {
+  let mensaje;
+  if (person.age >= 18) {
+    mensaje = "Es mayor de edad";
+  } else {
+    mensaje = "Es menor de edad";
+  }
+
+  console.log(mensaje);
+}
+
+itHasMoreThanEighteenLet(person);
